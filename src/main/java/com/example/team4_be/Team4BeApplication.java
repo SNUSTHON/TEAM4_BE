70458b1edd06510095cefe5146f1bd281271ae0d
hawardShin <joushin@student.42seoul.kr>
2024-09-07 22:55:51 +0900

    chore: Set gradle

--- src/main/java/com/example/team4_be/Team4BeApplication.java
+++ src/main/java/com/example/team4_be/Team4BeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Team4BeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Team4BeApplication.class, args);
     }
-
 }
