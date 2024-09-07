5a287e8adac2eb73e76f4fcc3a9cf9c5b9b52806
hawardShin <joushin@student.42seoul.kr>
2024-09-08 03:14:24 +0900

    Feat : Set Swagger

--- src/main/java/com/example/team4_be/Controller/UserController.java
+++ src/main/java/com/example/team4_be/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.team4_be.Controller;
 
 import com.example.team4_be.Dto.UserDto;
 import com.example.team4_be.Service.UserService;
+import com.example.team4_be.config.ServerProperties;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,22 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api")
+@Tag(name = "User", description = "유저 API")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private final ServerProperties serverProperties;
 
     @PostMapping(value = "/deceased", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Map<String, String>> addUser(
             @ModelAttribute UserDto userDto) {
-
+        System.out.println(serverProperties.getUrl());
         try {
             userService.addUser(userDto);  // 수정된 부분: 인스턴스 변수로 호출
-
             Map<String, String> response = new HashMap<>();
             response.put("status", "success");
             response.put("message", "Missing person added successfully");
--- src/main/java/com/example/team4_be/Dto/UserDto.java
+++ src/main/java/com/example/team4_be/Dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.team4_be.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,10 +8,15 @@ import java.time.LocalDate;
 
 @Data
 public class UserDto {
+    @Schema(description = "유저 ID", example = "1")
     private Long userId;
+    @Schema(description = "유저 이름", example = "홍길동")
     private String name;
+    @Schema(description = "유저 관계", example = "친구")
     private String relation;
+    @Schema(description = "유저 생일", example = "1990-01-01")
     private LocalDate birth;
+    @Schema(description = "유저 추모일", example = "2021-01-01")
     private LocalDate memorialDate;
     private MultipartFile profileImage;
 }
