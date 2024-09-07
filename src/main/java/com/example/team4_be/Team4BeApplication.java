26562b5afbe931ccaffa28b8afdfc7b2a5aa74ed
HUIJAEKO <xrhgmlwox@naver.com>
2024-09-07 21:33:53 +0900

    first commit

---
+++ src/main/java/com/example/team4_be/Team4BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team4BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team4_be/Team4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team4_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
